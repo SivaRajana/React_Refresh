8fdab3859327f777b63461fc0cdfc25a3d59dcd5
Siva Rajana <30532819+SivaRajana@users.noreply.github.com>
2022-02-17 12:04:13 +0530

    added event listeners to button on counter component no need specify const or let infront of function name which is used for ecent listeners.(check counter component)

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import "./styles.css";
 import { Component } from "react";
 import Counter from "./components/counter/index";
 
-class Practice extends Component {
+class App extends Component {
   render() {
-    const { candidate } = this.props;
     return (
       <div>
-        <h1>Hey {candidate}, this is just refresh project</h1>
+        <h1>Counter For React Syntax Refresh</h1>
         <Counter />
       </div>
     );
   }
 }
-export default Practice;
+export default App;
--- src/components/counter/index.js
+++ src/components/counter/index.js
@@ -1,13 +1,22 @@
 import { Component } from "react";
+import "./index.css";
 
 class Counter extends Component {
+  incrementClick = () => {
+    console.log("Increment button clicked!");
+  };
+  decrementClick = () => {
+    console.log("Decrement button clicked");
+  };
+
   render() {
     return (
-      <div>
+      <div className="text-center">
         <h1>Counter</h1>
+        <p>0</p>
         <div>
-          <button> Button1 </button>
-          <button> Button1 </button>
+          <button onClick={this.incrementClick}> Increase </button>
+          <button onClick={this.decrementClick}> Decrease </button>
         </div>
       </div>
     );
