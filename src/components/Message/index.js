e7ffd02ca07ebb32284e60d656de5faa78e2fef3
Siva Rajana <30532819+SivaRajana@users.noreply.github.com>
2022-02-17 20:50:31 +0530

    conditional rendering 2nd way

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -2,22 +2,20 @@ import { Component } from "react";
 import "./index.css";
 
 class Message extends Component {
-  state = { limit: true };
-
-  danger_indication = () => {
-    const { limit } = this.state;
-    if (limit == false) {
-      return <button>Safe</button>;
-    } else {
-      return <button> Danger </button>;
-    }
-  };
+  state = { limit_exceeded: true };
 
   render() {
+    const { limit_exceeded } = this.state;
+    let danger_indication;
+    if (limit_exceeded == false) {
+      danger_indication = <button>Safe</button>;
+    } else {
+      danger_indication = <button> Danger </button>;
+    }
     return (
       <div className="text-cen-msg">
         <h1> Message will be shown Here </h1>
-        {this.danger_indication()}
+        {danger_indication}
       </div>
     );
   }
