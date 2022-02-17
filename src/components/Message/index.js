c5079c934247d7bc6e9599703d22a5a996d1e519
Siva Rajana <30532819+SivaRajana@users.noreply.github.com>
2022-02-17 22:50:04 +0530

    && method used in place of ternary operator

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -4,17 +4,12 @@ import "./index.css";
 class Message extends Component {
   state = { limit: true };
   render() {
-    const danger_indication = () => {
-      const { limit } = this.state;
-      if (limit == false) {
-        return <button>Safe</button>;
-      }
-      return <button> Danger </button>;
-    };
+    const { limit } = this.state;
     return (
       <div className="text-cen-msg">
-        <h1> Message will be shown Here </h1>
-        {danger_indication()}
+        <h1> Safe Level </h1>
+        {limit && <p> Exceeding....</p>}
+        {!limit && <p> not exceeding..don't worry....</p>}
       </div>
     );
   }
