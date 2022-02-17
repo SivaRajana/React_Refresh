6c6ce5e7b175ef3356bf2aa1a7d42ecad8a08540
Siva Rajana <30532819+SivaRajana@users.noreply.github.com>
2022-02-17 13:22:14 +0530

    state property added class component used when we require state along with props.

--- src/components/counter/index.js
+++ src/components/counter/index.js
@@ -2,18 +2,29 @@ import { Component } from "react";
 import "./index.css";
 
 class Counter extends Component {
+  state = { count: 0 };
+
   incrementClick = () => {
-    console.log("Increment button clicked!");
+    // console.log("Increment button clicked!");
+    this.setState((prev) => {
+      console.log(`The Value is ${prev.count}`);
+      return { count: prev.count + 1 };
+    });
   };
   decrementClick = () => {
     console.log("Decrement button clicked");
+    this.setState((prev) => {
+      console.log(`The Value is ${prev.count}`);
+      return { count: prev.count - 1 };
+    });
   };
 
   render() {
+    const { count } = this.state;
     return (
       <div className="text-center">
         <h1>Counter</h1>
-        <p>0</p>
+        <p className="counter-text">{count}</p>
         <div>
           <button onClick={this.incrementClick}> Increase </button>
           <button onClick={this.decrementClick}> Decrease </button>
