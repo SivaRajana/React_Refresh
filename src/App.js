ca0e6b16b74d98074ed2220d10fe49a0938782b0
Siva Rajana <30532819+SivaRajana@users.noreply.github.com>
2022-02-17 11:39:25 +0530

    added counter component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./styles.css";
 import { Component } from "react";
+import Counter from "./components/counter/index";
 
 class Practice extends Component {
   render() {
     const { candidate } = this.props;
-    return <h1>Hey {candidate}, this is just refresh project</h1>;
+    return (
+      <div>
+        <h1>Hey {candidate}, this is just refresh project</h1>
+        <Counter />
+      </div>
+    );
   }
 }
 export default Practice;
---
+++ src/components/counter/index.js
@@ -0,0 +1,17 @@
+import { Component } from "react";
+
+class Counter extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Counter</h1>
+        <div>
+          <button> Button1 </button>
+          <button> Button1 </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Counter;
