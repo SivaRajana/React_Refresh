fc4851b237343aa899a75d4c2f4bfbd7259d5f54
Siva Rajana <30532819+SivaRajana@users.noreply.github.com>
2022-02-17 16:08:20 +0530

    added if condition can't increase count more than 10 and can't decrease less than 0.

--- src/components/counter/index.js
+++ src/components/counter/index.js
@@ -2,28 +2,40 @@ import { Component } from "react";
 import "./index.css";
 
 class Counter extends Component {
-  state = { count: 0 };
+  state = {
+    count: 0,
+    counterDisplay: " Counter",
+    styleCounter: "counter-text"
+  };
 
   incrementClick = () => {
     // console.log("Increment button clicked!");
     this.setState((prev) => {
       console.log(`The Value is ${prev.count}`);
-      return { count: prev.count + 1 };
+      if (prev.count < 10) {
+        return {
+          count: prev.count + 1,
+          counterDisplay: "Increasing...",
+          styleCounter: "red-color"
+        };
+      }
     });
   };
   decrementClick = () => {
     console.log("Decrement button clicked");
     this.setState((prev) => {
       console.log(`The Value is ${prev.count}`);
-      return { count: prev.count - 1 };
+      if (prev.count > 0) {
+        return { count: prev.count - 1, counterDisplay: "Decreasing.." };
+      }
     });
   };
 
   render() {
-    const { count } = this.state;
+    const { count, counterDisplay, styleCounter } = this.state;
     return (
       <div className="text-center">
-        <h1>Counter</h1>
+        <h1 className={styleCounter}>{counterDisplay}</h1>
         <p className="counter-text">{count}</p>
         <div>
           <button onClick={this.incrementClick}> Increase </button>
