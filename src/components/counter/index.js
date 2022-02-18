18c51570439702598854d3dca0a7f88c965918f1
Siva Rajana <30532819+SivaRajana@users.noreply.github.com>
2022-02-18 11:11:49 +0530

    initial commit

--- src/components/counter/index.js
+++ src/components/counter/index.js
@@ -2,45 +2,22 @@ import { Component } from "react";
 import "./index.css";
 
 class Counter extends Component {
-  state = {
-    count: 0,
-    counterDisplay: " Counter",
-    styleCounter: "counter-text"
-  };
-
-  incrementClick = () => {
-    // console.log("Increment button clicked!");
-    this.setState((prev) => {
-      console.log(`The Value is ${prev.count}`);
-      if (prev.count < 10) {
-        return {
-          count: prev.count + 1,
-          counterDisplay: "Increasing...",
-          styleCounter: "red-color"
-        };
-      }
-    });
-  };
-  decrementClick = () => {
-    console.log("Decrement button clicked");
-    this.setState((prev) => {
-      console.log(`The Value is ${prev.count}`);
-      if (prev.count > 0) {
-        return { count: prev.count - 1, counterDisplay: "Decreasing.." };
-      }
-    });
-  };
+  state = { count: "start", isEven: null };
 
   render() {
-    const { count, counterDisplay, styleCounter } = this.state;
+    const modify = (prev) => {
+      const randNum = Math.ceil(Math.random() * 100);
+      this.setState((prev) => {
+        const x = randNum % 2 == 0 ? "Even" : "ODD";
+        return { count: randNum, isEven: x };
+      });
+    };
+    const { count, isEven } = this.state;
     return (
-      <div className="text-center">
-        <h1 className={styleCounter}>{counterDisplay}</h1>
-        <p className="counter-text">{count}</p>
-        <div>
-          <button onClick={this.incrementClick}> Increase </button>
-          <button onClick={this.decrementClick}> Decrease </button>
-        </div>
+      <div>
+        <h1>{isEven}</h1>
+        <h2>{count} </h2>
+        <button onClick={modify}>MODIFY Number</button>
       </div>
     );
   }
